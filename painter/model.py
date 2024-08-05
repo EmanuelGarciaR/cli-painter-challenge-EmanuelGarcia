b73fbc0660118f602b1bb6b5ff873eb7f2f70461
Ema <emanuelgarciarios@gmail.com>
2024-08-05 12:45:00 -0500

    Create Circle class and area method

--- painter/model.py
+++ painter/model.py
@@ -1,5 +1,20 @@
 # TODO: Add code here
+# IMPORTS
+import math
+
+
 class Point:
     def __init__(self, x:float, y:float):
         self.x = x
         self.y = x
+#Una clase Circle con los atributos center de tipo Point y
+# radius de tipo float. Ambos atributos deben ser inicializados con parámetros en el constructor.
+class Circle:
+    def __init__(self, center:Point, radius:float):
+        self.center = center
+        self.radius = radius
+
+#Method circle area
+    def area(self) -> float:
+        area_circle = math.pi * self.radius**2
+        return area_circle
